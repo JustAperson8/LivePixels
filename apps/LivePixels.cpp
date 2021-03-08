62af5ad81c70c93b18626da6fd4114132e248c3d
daniil <dan.naumihin@yandex.ru>
2021-03-08 21:48:08 +0300

    message

--- apps/LivePixels.cpp
+++ apps/LivePixels.cpp
@@ -1,45 +1,30 @@
 #include <vector>
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include "LivePixels/constants.hpp"
 #include "LivePixels/math.hpp"
 #include "LivePixels/Ray.hpp"
 #include "LivePixels/SDFs.hpp"
 
-class Bublic : public lp::SDFs::ISDF
-{
-private:
-    float m_radius, m_Radius;
-public:
-    Bublic(float radius = 10, float Radius = 20)
-        : m_radius(radius), m_Radius(Radius)
-    {}
-
-    ~Bublic() = default;
-
-    [[nodiscard]] virtual float draw(const lp::Quaternion<float> &quater) const 
-    {
-        // auto Quater(quater);
-        // Quater.setX(static_cast<int>(Quater.getX()) % 30);
-        // Quater.setY(static_cast<int>(Quater.getY()) % 30);
-        // Quater.setZ(static_cast<int>(Quater.getZ()) % 30);
-        // return std::sqrt( std::pow(std::sqrt(Quater.getX() * Quater.getX() + Quater.getZ() * Quater.getZ()) - m_Radius, 2) + Quater.getY() * Quater.getY()) - m_radius;
-        return std::sqrt( std::pow(std::sqrt(quater.getX() * quater.getX() + quater.getZ() * quater.getZ()) - m_Radius, 2) + quater.getY() * quater.getY()) - m_radius;
-    }
-};
-
 class CubeSDF : public lp::SDFs::ISDF
 {
-private:
-    lp::Quaternion<float> cube;
+public:
+    float side;
+    lp::Quaternion<float> movings, rotation; 
 
 public:
-    CubeSDF (lp::Quaternion<float> cube)
-        : cube(cube)
-    {
-    } 
+    CubeSDF (float side, lp::Quaternion<float> movings, lp::Quaternion<float> rotation)
+        : side(std::abs(side)), movings(movings), rotation(rotation)
+    {} 
     
     virtual float draw(const lp::Quaternion<float> &quater) const override
     {
+        auto result = quater - movings;
+        lp::Quaternion<float> rx({std::cos(rotation.getX()) / 2, std::sin(rotation.getX() / 2), 0, 0});
+        lp::Quaternion<float> ry({std::cos(rotation.getY()) / 2, 0, std::sin(rotation.getY() / 2), 0});
+        lp::Quaternion<float> rz({std::cos(rotation.getZ()) / 2, 0, 0, std::sin(rotation.getZ()) / 2});
+        result = rx * (ry * (rz * result * rz.inverse()) * ry.inverse()) * rx.inverse();
+        return std::max(std::max(std::abs(result.getX()), std::abs(result.getY())), std::abs(result.getZ())) - side / 2;
     }
 
     ~CubeSDF () = default;
@@ -48,11 +33,25 @@ public:
 
 int main() 
 { 
-    size_t w = 200, h = 200; 
+    float test = 0;
+    size_t w = 50, h = 50; 
     // va is a view angle
     float vaw = 90, vah = 90; 
-    std::vector<lp::SDFs::ISDF *> mapForRender(1, nullptr);
-    mapForRender[0] = new lp::SDFs::GroupOfSDFs({new lp::SDFs::Sphere({0, 0, 0, 0}, 10), new lp::SDFs::Sphere({0, 0, -10, 0}, 10)}, {new Bublic(10, 12)});
+    std::vector<lp::SDFs::ISDF *> mapForRender(3, nullptr);
+
+    mapForRender[0] = new lp::SDFs::GroupOfSDFs({
+        new CubeSDF(6, {0, 0, 0, 0}, {0, M_PI, 0, 0}), 
+        new lp::SDFs::Sphere({0, 0, -1.6, -1.6}, 3),
+        new lp::SDFs::Sphere({0, 0, -1.6, 1.6}, 3)
+        }, {                
+        new CubeSDF(20, {0, -10, 0, 0}, {0, 0, 0, 0}),
+        new CubeSDF(20, {0, 10, 0, 0}, {0, 0, 0, 0}),
+        });
+
+    // mapForRender[0] = new lp::SDFs::GroupOfSDFs({new CubeSDF(6, {0, 10, 10, 10}, {0, 0, 0, 0})}, {});
+    mapForRender[1] = new lp::SDFs::GroupOfSDFs({new CubeSDF(6, {0, 5, 5, 0}, {0, 0, 0, 0})}, {new lp::SDFs::Sphere({0, 5, 4, 0}, 5)});
+    mapForRender[2] = new lp::SDFs::Sphere({0, 0, -2, -2}, 2);
+
     lp::Quaternion<float> startRotator(0, 1, 0, 0),
         rot(std::cos(lp::angleToRadian(1)), 0, std::sin(lp::angleToRadian(1)), 0);
     lp::Ray ray(mapForRender, {0, 0, 0, 0}, 200, 7);
@@ -61,6 +60,9 @@ int main()
     sf::RectangleShape pixel({1, 1});
     while (window.isOpen())
     {
+        test += 0.1;
+        dynamic_cast<CubeSDF*>(dynamic_cast<lp::SDFs::GroupOfSDFs*>(mapForRender[1])->m_visibleSDFs[0])->rotation.setX(test);
+        
         sf::Event event{};
         while (window.pollEvent(event))
         {
@@ -105,6 +107,11 @@ int main()
             ray.setPosition(ray.getPosition() + lp::Quaternion<float>(0, 0, -1, 0));
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
             ray.setPosition(ray.getPosition() + lp::Quaternion<float>(0, 0, 1, 0));
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+            ray.setPosition(ray.getPosition() + lp::Quaternion<float>(0, 0, 0, 1));
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::X))
+            ray.setPosition(ray.getPosition() + lp::Quaternion<float>(0, 0, 0, -1));
+
         window.clear();
         for (float x = 0; x < w; ++x)
         {
@@ -121,7 +128,10 @@ int main()
                        std::sin(lp::angleToRadian((y * vah / h) / 2))
                 );
 
-                float dist = ray.rayMarching(r2 * (r1 * startRotator * r1.inverse()) * r2.inverse());
+                lp::Quaternion<float> wtf(r2 * (r1 * startRotator * r1.inverse()) * r2.inverse());
+                float dist = ray.rayMarching(wtf);
+                if (dist < 2000)
+                    std::cout << wtf << std::endl;
                 if (dist != lp::notFound)
                 {
                     pixel.setPosition(x, y);
